b31b7b712f2cae98e0bfc148ce0ef6aa1e193de1
anwarzebbangash <anwarzebkhanbangash655@gmail.com>
2026-09-21 16:48:50 +0500

    Explicitly pass mysql2 as dialectModule to Sequelize

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,10 +1,11 @@
-import "mysql2";
+import mysql2 from "mysql2";
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'mysql://root:root@localhost:3306/tech_blog',
   {
     dialect: 'mysql',
+    dialectModule: mysql2,
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
       max: 5,
